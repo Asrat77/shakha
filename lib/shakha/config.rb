37ec317024cc7fa5e922cabbc5d97e9e10b58b43
Asrat <asrat@example.com>
2026-05-10 09:35:18 +0300

    Add Config class

---
+++ lib/shakha/config.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Shakha
+  class Config
+    attr_accessor :app_origin,
+                  :service_url,
+                  :service_secret,
+                  :google_client_id,
+                  :google_client_secret,
+                  :issuer,
+                  :session_lifetime
+
+    def initialize
+      @session_lifetime = 30.days
+      @issuer = "https://shakha.dev"
+    end
+
+    def embedded?
+      service_url.blank?
+    end
+
+    def service_base_url
+      return app_origin if embedded?
+
+      service_url.chomp("/")
+    end
+
+    def client_id
+      return @client_id if defined?(@client_id)
+
+      origin = URI.parse(app_origin).origin
+      @client_id = "origin:#{origin}"
+    end
+
+    def audience
+      client_id
+    end
+  end
+end
