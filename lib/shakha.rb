2e3dcae3adff409d34d20309fd039881742e486c
Asrat <asrat@example.com>
2026-05-20 14:32:34 +0300

    Wire providers into require chain, bump to v0.3.0
    Added require 'shakha/providers' to main shakha.rb.
    Architecture refactor complete: SPA-first OAuth broker with
    Google + GitHub providers, cookie + bearer auth, token redirect flow.

--- lib/shakha.rb
+++ lib/shakha.rb
@@ -7,6 +7,7 @@ require "shakha/pkce"
 require "shakha/rate_limiter"
 require "shakha/error_handler"
 require "shakha/controller_helpers"
+require "shakha/providers"
 require "shakha/engine"
 
 module Shakha
