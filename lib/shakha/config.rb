87b00af87a43e552967e7f27243a0fa43d97f79c
Asrat <asrat@example.com>
2026-05-20 14:23:28 +0300

    Simplify Config: remove service infrastructure attrs, add provider config
    Removed: service_url, service_secret, issuer, signing_key, verification_key,
    key_id, service_base_url, client_id, audience
    
    Added: providers array, github_client_id, github_client_secret

--- lib/shakha/config.rb
+++ lib/shakha/config.rb
@@ -3,43 +3,19 @@
 module Shakha
   class Config
     attr_accessor :app_origin,
-                  :service_url,
-                  :service_secret,
                   :google_client_id,
                   :google_client_secret,
-                  :issuer,
+                  :github_client_id,
+                  :github_client_secret,
+                  :providers,
                   :session_lifetime,
-                  :signing_key,
-                  :verification_key,
-                  :key_id,
                   :rate_limiting_enabled,
                   :allowed_redirect_origins
 
     def initialize
       @session_lifetime = 30.days
-      @issuer = "https://shakha.dev"
       @rate_limiting_enabled = false
-    end
-
-    def embedded?
-      service_url.blank?
-    end
-
-    def service_base_url
-      return app_origin if embedded?
-
-      service_url.chomp("/")
-    end
-
-    def client_id
-      return @client_id if defined?(@client_id)
-
-      origin = URI.parse(app_origin).origin
-      @client_id = "origin:#{origin}"
-    end
-
-    def audience
-      client_id
+      @providers = [:google]
     end
   end
 end
